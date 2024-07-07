710f1f66bde7ae106acef4b04329dd537f145843
Sagar Bhoge <sagarbhoge2003@gmail.com>
2024-07-07 10:00:22 +0530

    commit

---
+++ src/assets/sections/Hero/Hero.jsx
@@ -0,0 +1,22 @@
+import './HeroStyles.module.css'
+import React from 'react'
+
+function Hero() {
+  return (
+    <section>
+        <div className="hero-container">
+
+            <h1>Hello World !!!</h1>
+        </div>
+
+
+
+
+
+
+
+    </section>
+  )
+}
+
+export default Hero
